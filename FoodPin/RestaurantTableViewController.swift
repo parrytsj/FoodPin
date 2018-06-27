7d95c6a3b821c4da45f6afd661e6be7a4ebed6fb
parrytsj <parry.tsj@gmail.com>
2018-06-27 18:10:48 +0600

    Updated images from placeholder

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -12,10 +12,17 @@ class RestaurantTableViewController: UITableViewController {
 
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl",
                            "Petite Oyster", "For Kee Restaurant", "Po's Atelier",
-                           "Bourke Street Bakery", "Haighís Chocolate", "Palomino Espresso",
+                           "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso",
                            "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf",
                            "Five Leaves", "Cafe Lore", "Confessional", "Barrafina",
                            "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl",
+                           "petiteoyster", "forkeerestaurant", "posatelier",
+                           "bourkestreetbakery", "haighschocolate", "palominoespresso",
+                           "upstate", "traif", "grahamavenuemeats", "wafflewolf",
+                           "fiveleaves", "cafelore", "confessional", "barrafina",
+                           "donostia", "royaloak", "caskpubkitchen"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,7 +58,7 @@ class RestaurantTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         
         cell.textLabel?.text = restaurantNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: "restaurant")
+        cell.imageView?.image = UIImage(named: restaurantImages[indexPath.row])
         
         return cell
     }
