476f4b03e8bafc83c95685996b96563d4cab67aa
parrytsj <parry.tsj@gmail.com>
2018-07-15 11:44:40 +0600

    Colour change for delete and share button on swipe. Issue with share instance.

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -87,10 +87,21 @@ class RestaurantTableViewController: UITableViewController {
                 activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
             } else {
                 activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+                
+            // Change colour of delete and share actions
+            deleteAction.backgroundColor = UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 60.0/255.0, alpha: 1.0)
+            deleteAction.image = UIImage(named: "delete")
+                
+            /*
+            shareAction.backgroundColour = UIColor(red: 254.0/255.0, green: 149.0/255.0, blue: 38.0/255.0, alpha: 1.0)
+            shareAction.image = UIImage(named: "share")
+            */
+            
             }
             
             self.present(activityController, animated: true, completion: nil)
             completionHandler(true)
+    
         }
         
         let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction, shareAction])
