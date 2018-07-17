fb1f14145923acba4c311f754eabd13f8481c771
parrytsj <parry.tsj@gmail.com>
2018-07-17 17:10:54 +0600

    Navigation Controller large titles preferred

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -22,6 +22,7 @@ class RestaurantTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
         
     }
     
